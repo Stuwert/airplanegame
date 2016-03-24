d637103c05175dc26b5c9e26fe681d57789e353d
Stuwert <stuart@urback.net>
2016-03-23 22:39:49 -0600

    Initalize stuff and start socket tests

---
+++ test/test-chat-server.js
@@ -0,0 +1,70 @@
+var should = require('chai').should();
+var io = require('socket.io-client');
+
+var socketURL = 'http://localhost:3000';
+
+var options = {
+  transports: ['websocket'],
+  'force new connection' : true
+}
+
+var chatUser1 = {'name' : 'Tom'};
+var chatUser2 = {'name' : 'Sally'};
+var chatUser3 = {'name' : 'Dana'};
+
+
+describe('Chat Server', function(){
+  it('Should broadcast new user to all users', function(done){
+    var client1 = io.connect(socketURL, options);
+
+    client1.on('connect', function(data){
+      client1.emit('game1', chatUser1);
+
+      var client2 = io.connect(socketURL, options);
+
+      client2.on('connect', function(data){
+        client2.emit('game1', chatUser2);
+      })
+
+      client2.on('new user', function(usersName){
+        usersName.should.equal(chatUser2.name + " has joined.");
+        client2.disconnect();
+      });
+
+      var numUsers = 0;
+      client1.on('new user', function(usersName){
+        numUsers += 1;
+
+        if(numUsers ===2){
+          usersName.should.equal(chatUser2.name + " has joined.");
+          client1.disconnect();
+          done();
+        }else{
+          usersName.should.equal(chatUser1.name + " has joined.")
+        }
+      })
+
+    })
+
+  })
+  it('Should be able to broadcast messages', function(done){
+    var client1, client2, client3;
+    var message = "Hello World";
+    var messages = 0;
+
+    var checkMessage = function(client){
+      client.on('message', function(msg){
+        message.should.equal(msg);
+        client.disconnect();
+        messages++;
+        if (messages === 3){
+          done();
+        }
+      })
+    };
+
+    client1 = io.connect(socketURL, options);
+    checkMessage(client1);
+    
+  })
+})
