400c7806466e915391c444f396506463d19c4a5b
Stuart Urback <stuart@urback.net>
2016-03-24 21:54:44 -0600

    Complete basic sockets work

--- test/test-chat-server.js
+++ test/test-chat-server.js
@@ -73,137 +73,139 @@ describe('Game Server', function(){
   })
   it('Should be able to connect to a room', function(done){
     var clientz = io.connect(socketURL, options);
-    clientz.on('verify', function(response){
-
-    })
     clientz.on('connect', function(data){
-
+      clientz.emit('joinGame', chatUser1.name, "room1")
+      clientz.on('joined', function(response){
+        response.should.equal("you connected to room1")
+        clientz.disconnect();
+        done();
+      })
     })
   })
-  xit('Should be notified the number of players in a room', function(){
-
+  it('Should be notified the number of players in a room', function(done){
+    var clientz = io.connect(socketURL, options);
+    clientz.on('connect', function(data){
+      clientz.emit('joinGame', chatUser1.name, "room1")
+      clientz.on('roomInfo', function(response){
+        response.should.deep.equal(
+          {
+            "room1":
+            {
+              "users" : [
+                "Tom"
+              ],
+              "game" : [0, 0, 0]
+            },
+            "room2" : {
+              "users" : [],
+              "game" : [0, 0, 1]
+            },
+            "room3":{
+              "users" : [],
+              "game" : [1, 0, 0]
+            }
+          }
+        )
+        clientz.disconnect();
+        done();
+      })
+    })
   })
-  xit('Should not allow another user if there are already 2 users in the room', function(){
-
+  it('Should allow two players to connect to a room', function(done){
+    var client1 = io.connect(socketURL, options);
+    client1.on('connect', function(data){
+      client1.emit('joinGame', chatUser1.name, "room1")
+      client1.on('joined', function(response){
+        var client2 = io.connect(socketURL, options);
+        client2.on('connect', function(moreData){
+          client2.emit('joinGame', chatUser2.name, "room1");
+          client2.on('joined', function(client2Response){
+            client2Response.should.equal('you connected to room1');
+            client2.on('roomInfo', function(allRooms){
+              allRooms.room1.users.should.deep.equal(["Tom", "Sally"]);
+              client1.disconnect();
+              client2.disconnect();
+              done();
+            })
+          })
+        })
+      })
+    })
+  })
+  it('Should not allow another user if there are already 2 users in the room', function(done){
+    var client1 = io.connect(socketURL, options);
+    client1.on('connect', function(data){
+      client1.emit('joinGame', chatUser1.name, "room1")
+      client1.on('joined', function(response){
+        var client2 = io.connect(socketURL, options);
+        client2.on('connect', function(moreData){
+          client2.emit('joinGame', chatUser2.name, "room1");
+          client2.on('joined', function(client2Response){
+            var client3 = io.connect(socketURL, options);
+            client3.on('connect', function(client3Response){
+              client3.emit('joinGame', chatUser3.name, "room1");
+              client3.on('roomResponse', function(roomStatement){
+                roomStatement.should.equal("could not connect to room");
+                client1.disconnect();
+                client2.disconnect();
+                client3.disconnect();
+                done();
+              })
+            })
+          })
+        })
+      })
+    })
+  })
+  it('Should get information about the current game state', function(done){
+    var client1 = io.connect(socketURL, options);
+    client1.on('connect', function(data){
+      client1.emit('joinGame', chatUser1.name, "room1")
+      client1.on('joined', function(response){
+        var client2 = io.connect(socketURL, options);
+        client2.on('connect', function(moreData){
+          client2.emit('joinGame', chatUser2.name, "room1");
+          client2.on('joined', function(client2Response){
+            client2.emit('updateGame', "Bing Bong");
+            client1.on('updateGame', function(gameResponse){
+              console.log("test");
+              gameResponse.should.deep.equal([0, 0, 0])
+              client2.on('updateGame', function(otherGameResponse){
+                otherGameResponse.should.deep.equal([0, 0, 0])
+                client1.disconnect();
+                client2.disconnect();
+                done();
+              })
+            })
+          })
+        })
+      })
+    })
+  })
+  it('Should not get information about another games game state', function(done){
+    var seesit = false;
+    var client1 = io.connect(socketURL, options);
+    client1.on('connect', function(data){
+      client1.emit('joinGame', chatUser1.name, "room1")
+      client1.on('joined', function(response){
+        var client2 = io.connect(socketURL, options);
+        client2.on('connect', function(moreData){
+          client2.emit('joinGame', chatUser2.name, "room1");
+          client2.on('joined', function(client2Response){
+            var client3 = io.connect(socketURL, options);
+            client3.on('connect', function(thing){
+              client2.emit('updateGame', "Bing Bong");
+              client3.on('updateGame', function(thingy){
+                seesit = true;
+                seesit.should.equal(false);
+              })
+              client1.disconnect();
+              client2.disconnect();
+              setTimeout(done, 400);
+            })
+          })
+        })
+      })
+    })
   })
 })
-
-
-// describe('Chat Server', function(){
-//   it('Should broadcast new user to all users', function(done){
-//     var client1 = io.connect(socketURL, options);
-//
-//     client1.on('connect', function(data){
-//       client1.emit('game1', chatUser1);
-//
-//       var client2 = io.connect(socketURL, options);
-//
-//       client2.on('connect', function(data){
-//         client2.emit('game1', chatUser2);
-//       })
-//
-//       client2.on('new user', function(usersName){
-//         usersName.should.equal(chatUser2.name + " has joined.");
-//         client2.disconnect();
-//       });
-//
-//       var numUsers = 0;
-//       client1.on('new user', function(usersName){
-//         numUsers += 1;
-//
-//         if(numUsers ===2){
-//           usersName.should.equal(chatUser2.name + " has joined.");
-//           client1.disconnect();
-//           done();
-//         }else{
-//           usersName.should.equal(chatUser1.name + " has joined.")
-//         }
-//       })
-//
-//     })
-//
-//   })
-//   it('Should be able to broadcast messages', function(done){
-//     var client1, client2, client3;
-//     var message = "Hello World";
-//     var messages = 0;
-//
-//     var checkMessage = function(client){
-//       client.on('message', function(msg){
-//         message.should.equal(msg);
-//         client.disconnect();
-//         messages++;
-//         if (messages === 3){
-//           done();
-//         };
-//       });
-//     };
-//
-//     client1 = io.connect(socketURL, options);
-//     checkMessage(client1);
-//
-//     client1.on('connect', function(data){
-//       client2 = io.connect(socketURL, options);
-//       checkMessage(client2);
-//
-//       client2.on('connect', function(data){
-//         client3 = io.connect(socketURL, options);
-//         checkMessage(client3);
-//
-//         client3.on('connect', function(data){
-//           client2.send(message);
-//         })
-//       })
-//     })
-//
-//   })
-//
-//   it('Should be able to send private messages', function(done){
-//     var client1, client2, client3;
-//     var message = {to: chatUser1.name, txt: 'Private Hello World'};
-//     var messages = 0;
-//
-//     var completeTest = function(){
-//       messages.should.equal(1);
-//       client1.disconnect();
-//       client2.disconnect();
-//       client3.disconnect();
-//       done();
-//     }
-//
-//     var checkPrivateMessage = function(client){
-//       client.on('private message', function(msg){
-//         message.txt.should.equal(msg.txt);
-//         msg.from.should.equal(chatUser3.name);
-//         messages++;
-//         if(client === client1){
-//           setTimeout(completeTest, 40);
-//         }
-//       })
-//     }
-//
-//     client1 = io.connect(socketURL, options);
-//     checkPrivateMessage(client1);
-//
-//     client1.on('connect', function(data){
-//       client1.emit('game1', chatUser1);
-//       client2 = io.connect(socketURL, options);
-//       checkPrivateMessage(client2);
-//
-//       client2.on('connect', function(data){
-//         client2.emit('game1', chatUser2);
-//         client3 = io.connect(socketURL, options);
-//         checkPrivateMessage(client3);
-//
-//         client3.on('connect', function(data){
-//           client3.emit('game1', chatUser3);
-//           client3.emit('private message', message);
-//         })
-//
-//       })
-//
-//     })
-//
-//   })
-//
-// })
